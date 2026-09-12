8d1757a2ee0940084424aa0eb3193db8f507ac3b
Frank <774frank8@gmail.com>
2026-09-12 20:11:01 +0800

    auto: 2026-09-12 20:11 session changes

--- list_browser.ps1
+++
@@ -1,1 +0,0 @@
-Get-Process | Where-Object {$_.MainWindowHandle -ne 0 -and $_.MainWindowTitle -match 'chrome|edge|firefox|browser'} | Select-Object ProcessName,MainWindowTitle,Id | Format-Table -AutoSize
